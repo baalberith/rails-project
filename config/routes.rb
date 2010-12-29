9adbcfd6a0a139dd79abf37d57ab75462eb9fd90
Baalberith <spr@spr-laptop.(none)>
2010-12-29 18:06:20 +0100

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Project::Application.routes.draw do
+  match '/about' => 'pages#about'
+  
+  root :to => 'pages#home'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
