c235f4c329c9ebe091d442ba7fa36146483da899
Baalberith <spr@spr-laptop.(none)>
2010-12-29 17:12:27 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project::Application.config.secret_token = '6b96e17b9de06aece211b1421e590f03c5a12993a43b59d39362853a1e299719723be65ee1e96de09a4e179dfba84cffe1d4eca39c2533e9e6d61f6834b2fe86'
